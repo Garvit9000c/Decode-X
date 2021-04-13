bea0838692467e75bd806aa99adf4224f3a76104
Garvit9000c <909garvit@gmail.com>
2021-04-13 21:17:30 +0530

    Update

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
+import Summary as sp
 
 @app.route('/')
 def home():
@@ -15,7 +16,7 @@ def get_data():
 
 @app.route('/success/<name>')
 def success(name):
-    return "<xmp>" + str(name) + " </xmp> "
+    return "<xmp>" + sp.generate_summary(name,5) + " </xmp> "
 
 
 if __name__ == '__main__' :
