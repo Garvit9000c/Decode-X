92aa069fd01d46ffe003844126cdffe8082ef869
Garvit9000c <909garvit@gmail.com>
2021-04-14 10:27:12 +0530

    up

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ def get_data():
 
 @app.route('/success/<name>')
 def success(name):
-    summary = generate_summary(name,5)
+    summary = generate_summary(name,3)
     # dynamic HTML document
     html = """<html>
     <head>
