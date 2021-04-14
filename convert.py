f383c94a0dc6d2f7352edabd0a5ee801dd953282
Garvit9000c <909garvit@gmail.com>
2021-04-14 23:58:39 +0530

    0.4

--- convert.py
+++ convert.py
@@ -1,7 +1,7 @@
 from PIL import Image
 from pytesseract import pytesseract
-path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-pytesseract.tesseract_cmd = path_to_tesseract
+#path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+#pytesseract.tesseract_cmd = path_to_tesseract
 def sp(t):
 	s=""
 	for i in t:
