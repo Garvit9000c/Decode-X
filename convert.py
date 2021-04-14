c8691cb667743ee0d63d17672b02fdd2297a1d2e
Garvit9000c <909garvit@gmail.com>
2021-04-14 20:15:28 +0530

    0.2

--- convert.py
+++ convert.py
@@ -1,16 +1,7 @@
 from PIL import Image
 from pytesseract import pytesseract
-  
-image_path = r"./img.png"
-  
-# Opening the image & storing it in an image object
-img = Image.open(image_path)
- 
-# Passing the image object to 
-# image_to_string() function
-# This function will
-# extract the text from the image
-text = pytesseract.image_to_string(img)
-  
-# Displaying the extracted text
-print(text[:-1])
+
+def con(image_path):
+	img = Image.open(image_path)
+	text = pytesseract.image_to_string(img)
+	return text[:-1])
