c914d46ef16390dc2cb2bacda6477430efd44834
Garvit9000c <909garvit@gmail.com>
2021-04-16 14:30:04 +0530

    deploy

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 @app.route('/')
 def home():
     return render_template('home.html')
