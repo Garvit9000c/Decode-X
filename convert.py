16baf5066629646acb66b4509bd2583f7bb7676b
Garvit9000c <909garvit@gmail.com>
2021-04-16 09:56:33 +0530

    deploy

--- convert.py
+++ convert.py
@@ -18,13 +18,14 @@ def Simplifier(text):
 	
 def Text_convertor(img_):
 	img=Image.open(img_)
-	Hindi = pytesseract.image_to_string(img,lang='hin')
+	#Hindi = pytesseract.image_to_string(img,lang='hin')
 	English = pytesseract.image_to_string(img)
-	if (len(Hindi.split())>len(English.split())):
-		text=Hindi
-	else:
-		text=English
-	return text
+	
+	#if (len(Hindi.split())>len(English.split())):
+	#	text=Hindi
+	#else:
+	#	text=English
+	return English
 
 
 def English(string):
