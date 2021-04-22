e8c90e0ff354d7f3600495cc4a8ae48b9ea36101
Mrityunjay <mrityunjayvyasmhl@gmail.com>
2021-04-22 16:22:37 +0530

    Correction

--- app.py
+++ app.py
@@ -62,9 +62,10 @@ def summary(name):
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"
             integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ="
             crossorigin="anonymous" />
-        
-            <link rel="stylesheet" href="static/header.css">
+            
             <link rel="stylesheet" href="static/summaryStyle.css">
+            <link rel="stylesheet" href="static/header.css">
+            
         
             <title>Result | DECODE-X</title>
         </head>
